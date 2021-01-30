1da35fb5e8afe8a44365b40c16ac999f1e3c927f
mandonglawrence <mandonglawrence@gmail.com>
2021-01-30 14:59:42 +0100

    checks

--- dist/test/test.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-describe("/", function () {
-    it("can get one correctly", function () {
-        expect(0).toBe(0);
-    });
-});
